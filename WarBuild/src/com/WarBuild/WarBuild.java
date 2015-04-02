f4d5ece8986c2e136bb5ca593959838eb721d316
kaushik030 <kaushik.143ap@gmail.com>
2015-04-02 12:40:47 +0530

    2nd commit of DynWebProj

---
+++ WarBuild/src/com/WarBuild/WarBuild.java
@@ -0,0 +1,14 @@
+package com.WarBuild;
+
+public class WarBuild {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("try commiting this");
+
+	}
+
+}
